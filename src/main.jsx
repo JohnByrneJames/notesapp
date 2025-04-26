d950ee37671a85d1791b6014792e55f01fe033fc
John B James <neardem1@gmail.com>
2025-04-26 14:55:51 +0100

    the notes app

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App subject="John" surname="Byrne 😏 🏳️‍🌈" />
   </StrictMode>,
 )
